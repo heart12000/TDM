042e64f57a00889ef2d2e54078fd9e05ed3d6023
heart1200 <heart1200@sina.com>
2018-08-17 18:14:17 +0800

    打开浏览器登陆

---
+++ test/tdm/base/Login.java
@@ -0,0 +1,28 @@
+/**
+ * 用于初始化登录tdm系统
+ **/
+
+package tdm.base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	public WebDriver driver;
+	
+	public void InitDriver(){
+		System.setProperty("webdriver.chrome.driver","D:/Selenium script recoder/chromedriver_win32_29/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		driver.get("https://testbpm.yktour.com.cn/");
+	}
+	
+public static void main(String[] args) {
+	Login login = new Login();
+	Login.InitDriver();
+	}
+
+}
